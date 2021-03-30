e139e4f353bccc4426cbcdce501603625a53735f
Marlo <marlo.baguyos@outlook.com>
2021-03-30 19:43:30 -0400

    update

--- organikaMain/organika/src/components/quantityPicker.jsx
+++ organikaMain/organika/src/components/quantityPicker.jsx
@@ -5,6 +5,7 @@ class QuantityPicker extends Component {
     render() { 
         return (
             <div>
+                <button className="btn btn-sm btn-primary">+</button>
                 <label>Qp</label>
             </div>
         );
