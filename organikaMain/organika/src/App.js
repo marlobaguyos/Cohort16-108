8c293256f17bdc2fe5f1d5e2287d2fcb6fb9e0ba
Marlo <marlo.baguyos@outlook.com>
2021-03-30 14:59:40 -0400

    update

--- organikaMain/organika/src/App.js
+++ organikaMain/organika/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import NavBar from './components/navBar';
 import Footer from './components/footer';
 
+
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <h1>Hello World!!!</h1>
+
       <Footer></Footer>
     </div>
   );
---
+++ organikaMain/organika/src/components/quantityPicker.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import QuantityPicker from './quantityPicker';
+
+class QuantityPicker extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <label>Qp</label>
+            </div>
+        );
+    }
+}
+
+export default QuantityPicker
