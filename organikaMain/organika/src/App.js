a4ab74e3f19986317b6b53f4600960f4c1b34051
Marlo <marlo.baguyos@outlook.com>
2021-03-29 22:20:20 -0400

    update

--- organikaMain/organika/src/App.js
+++ organikaMain/organika/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
-      <nav></nav>
+      <NavBar></NavBar>
       <h1>Hello World!!!</h1>
     </div>
   );
---
+++ organikaMain/organika/src/components/navBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                NavBar will be here!!
+            </div>
+        );
+    }
+}
+ 
+export default NavBar;
