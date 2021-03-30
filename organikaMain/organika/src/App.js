5ff728c92c989011e9f079f4af9276a742a73de9
Marlo <marlo.baguyos@outlook.com>
2021-03-30 09:50:57 -0400

    update

--- organikaMain/organika/src/App.js
+++ organikaMain/organika/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/navBar';
+import Footnote from './components/footnote';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <h1>Hello World!!!</h1>
+      <Footnote></Footnote>
     </div>
   );
 }
---
+++ organikaMain/organika/src/components/footnote.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footnote extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                This is were footnotes goes
+            </div>
+        );
+    }
+}
+ 
+export default Footnote;
