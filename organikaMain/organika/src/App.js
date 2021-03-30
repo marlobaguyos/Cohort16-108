c05e1e26b92deff9e2c98567630b3e99365acfd3
Marlo <marlo.baguyos@outlook.com>
2021-03-30 16:21:35 -0400

    update

--- organikaMain/organika/src/App.js
+++ organikaMain/organika/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './components/navBar';
 import Footer from './components/footer';
+import QuantityPicker from './components/quantityPicker';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <NavBar></NavBar>
       <h1>Hello World!!!</h1>
-
+      <QuantityPicker></QuantityPicker>
       <Footer></Footer>
     </div>
   );
