a07d01bcf9aae330108752788742822fad7e2f11
Marlo <marlo.baguyos@outlook.com>
2021-03-31 10:27:30 -0400

    update

--- organikaMain/organika/src/components/quantityPicker.jsx
+++ organikaMain/organika/src/components/quantityPicker.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 
 class QuantityPicker extends Component {
-    state = {  }
+    state = { 
+        quantity: 1,
+        name: 'Marlo'
+    }
     render() { 
         return (
             <div>
                 <button onClick={this.increaseQuantity} className="btn btn-sm btn-primary">++++</button>
-                <label>Click Me</label>
+                <label>{this.state.quantity}</label>
             </div>
         );
     }
 
-    increaseQuantity() {
+    increaseQuantity = () => {
         console.log("button clicked");
+        // this.state.quantity = 999; <-- DO NOT modify state directly
+
+        this.setState({quantity:999});
     }
 }
 
