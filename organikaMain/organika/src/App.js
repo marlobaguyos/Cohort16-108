7a28a1b286d65840677c38c0200dbf373619ffb3
Marlo <marlo.baguyos@outlook.com>
2021-03-31 13:05:35 -0400

    update

--- organikaMain/organika/src/App.js
+++ organikaMain/organika/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import NavBar from './components/navBar';
 import Footer from './components/footer';
-import QuantityPicker from './components/quantityPicker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import Product from './components/product';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+
+
+
       <h1 className="pogi">Hello World!!!</h1>
-      <QuantityPicker></QuantityPicker>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+
+
+
+
+
+
       <Footer></Footer>
     </div>
   );
---
+++ organikaMain/organika/src/components/product.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import QuantityPicker from './quantityPicker';
+
+class Product extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                
+
+                <QuantityPicker></QuantityPicker>
+            </div>
+        );
+    }
+}
+
+export default Product
