e837393c25fe3b7fe10716a5455bc9b359fe21f2
Marlo <marlo.baguyos@outlook.com>
2021-03-31 10:32:14 -0400

    update

--- organikaMain/organika/src/components/quantityPicker.jsx
+++ organikaMain/organika/src/components/quantityPicker.jsx
@@ -8,8 +8,9 @@ class QuantityPicker extends Component {
     render() { 
         return (
             <div>
-                <button onClick={this.increaseQuantity} className="btn btn-sm btn-primary">++++</button>
+                <button onClick={this.decreaseQuantity} className="btn btn-sm btn-primary">-</button>
                 <label>{this.state.quantity}</label>
+                <button onClick={this.increaseQuantity} className="btn btn-sm btn-primary">+</button>
             </div>
         );
     }
@@ -17,8 +18,17 @@ class QuantityPicker extends Component {
     increaseQuantity = () => {
         console.log("button clicked");
         // this.state.quantity = 999; <-- DO NOT modify state directly
+        
+        let current = this.state.quantity + 1; // read from the state 
+        this.setState({quantity:current});
+    }
 
-        this.setState({quantity:999});
+    decreaseQuantity = () => {
+        console.log("button clicked");
+        // this.state.quantity = 999; <-- DO NOT modify state directly
+        
+        let current = this.state.quantity - 1; // read from the state 
+        this.setState({quantity:current});
     }
 }
 
