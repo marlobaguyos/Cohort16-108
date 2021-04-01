c9858f1a688425244884eff5d65d0e2687d6df42
Marlo <marlo.baguyos@outlook.com>
2021-03-31 21:00:10 -0400

    update

--- organikaMain/organika/src/components/services/productService.js
+++ organikaMain/organika/src/components/services/productService.js
@@ -1,14 +1,12 @@
-var data = [
-  { 
-    id: "5f40a"
-    price: 12.32,
-    stock: 13,
-    title: "Cabbage",
-    image: "img-1.jpg",
-    discount: 5,
-    category: "Vegetable",
-  }
-];
+var data = [{
+  id: "5f40a",
+  price: 12.32,
+  stock: 13,
+  title: "Cabbage",
+  image: "img-1.jpg",
+  discount: 5,
+  category: "Vegetable",
+}];
 
 class ProductService {
   getProducts() {
